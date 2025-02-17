ec3706f0dfe3ab42c6a0b5f0376e863741bbf673
DreiLunar <vonandreilunar358@gmail.com>
2025-02-17 23:28:53 +0700

    Refactor FindTheMissingNumber script to improve functionality and add user input handling; add new test script for happy birthday function

--- Competitive Programming Practice/FindTheMissingNumber.py
+++ Competitive Programming Practice/FindTheMissingNumber.py
@@ -2,3 +2,14 @@ def missing_number():
     size = int(input("Enter size of array: "))
 
     n = size + 1
+
+    print("Enter elements:")
+    arr = [int(input()) for _ in range(size)]
+
+
+    total_sum = n * (n + 1) // 2
+    missing_number = total_sum - sum(arr)
+
+    print(f"Missing number: {missing_number}")
+
+missing_number()
---
+++ test/FunctionPractice.py
@@ -0,0 +1,7 @@
+def happy_birthday():
+    print("Maligayang kaarawan")
+    print("Hotdog diggididog")
+    print("MBTC!!")
+    print()
+
+happy_birthday()
